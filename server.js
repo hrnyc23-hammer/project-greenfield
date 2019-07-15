af4085c8c9ffa6ea7803ff1adcdd07ff1165e84a
Nick Pence <pencenj@gmail.com>
2019-07-15 15:06:13 -0400

    add listening to server

--- server.js
+++ server.js
@@ -2,4 +2,10 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+const port = 8888
+
 app.use(express.static(path.join(__dirname, '/dist')))
+
+app.listen(port, () => {
+    console.log('Listening on ', port)
+})
