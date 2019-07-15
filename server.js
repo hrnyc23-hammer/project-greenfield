4e5566cb94d81d9726561a018585d6fd39ec7301
Nick Pence <pencenj@gmail.com>
2019-07-15 15:18:05 -0400

    add check for deployed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-const port = 8888
+const port = process.env.port || 8888
 
 app.use(express.static(path.join(__dirname, '/dist')))
 
