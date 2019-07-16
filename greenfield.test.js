5a94224cb3986880d84520f62f87a69fd534909f
lancerutkin <lance.rutkin@gmail.com>
2019-07-16 15:21:19 -0400

    Add eslint

--- greenfield.test.js
+++ greenfield.test.js
@@ -1,6 +1,18 @@
-const { str } = require('./src/index.js');
+/* eslint-disable no-undef */
 const puppeteer = require('puppeteer');
 
+beforeAll(async () => {
+  puppeteer.launch().then((async browser => {
+    const page = await browser.newPage();
+    await page.goto('http://localhost:8888');
+    await browser.close();
+  }));
+});
+
 test('tests running', () => {
-  expect(str).toEqual('Welcome to Project Greenfield');
+  expect(true).toEqual(true);
+});
+
+test('can run e2e tests', async () => {
+  let selector = await page.$eval()
 });
