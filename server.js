8d2d521db0312acd11156bda6b226db0b74e4ef1
lancerutkin <lance.rutkin@gmail.com>
2019-07-17 17:30:23 -0400

    Remove server changes and uninstall unnecessary packages

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const cors = require('cors');
-const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 8888
 
-app.use(bodyParser.json());
-app.use(cors());
 app.use(express.static(path.join(__dirname, '/dist')))
 
 app.listen(port, () => {
