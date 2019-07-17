56747e7cde792ebcb07eaf7f7caa72fdf49858a4
lancerutkin <lance.rutkin@gmail.com>
2019-07-17 11:54:43 -0400

    Implement actions skeleton

---
+++ src/actions/fetchNewProduct.js
@@ -0,0 +1,8 @@
+import changeInfo from './changeInfo.js';
+import changeStyles from './changeStyles.js';
+import changeRelated from './changeRelated.js';
+import changeQA from './changeQA.js';
+import changeReviews from './changeReviews.js';
+import changeMeta from './changeMeta.js';
+
+import fetchProduct
