40e306c16a64497c9b9e1578876cc34378fb1380
Tom Cernera <tomcernera@gmail.com>
2019-07-17 16:42:33 -0400

    Removed console logs and set state to empty object

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -4,13 +4,12 @@ import React from 'react'
 const Overview = (props) => {
     return (
       <React.Fragment>
-        {console.log(props)}
         <div>Product Name : {props.info.name}</div>
         <div>Category : {props.info.category}</div>
         <div>Price : {props.info.price}</div>
         <div>
           {props.styles.results.map(style => {
-            return <div key={style.style_id}>{style.name} {console.log(style)}</div>
+            return <div key={style.style_id}>{style.name}</div>
           })}
         </div>
       </React.Fragment>
