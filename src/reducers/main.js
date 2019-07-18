8b293784c611f3828c88e415cb8fdc1e5685dd0e
Kevin Park <kevypark@Kevins-MacBook-Pro.local>
2019-07-18 11:48:04 -0400

    merge conflict fixed

--- src/reducers/main.js
+++ src/reducers/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { combineReducers } from 'redux';
 import related from './related.js';
 import overviewProductInfo from './overviewProductInfo.js';
@@ -8,20 +7,10 @@ import qaSearchEntry from './QAHandleSearchEntryReducer.js';
 import qa from './QAReducer.js';
 import meta from './metaReducer.js'
 import reviews from './reviewsReducer.js'
-=======
-import { combineReducers } from "redux";
-import related from "./related.js";
-import overviewProductInfo from "./overviewProductInfo.js";
-import overviewChangeStyles from "./overviewChangeStyles";
-import overviewChangeSelectedStyles from "./overviewChangeSelectedStyle.js";
-import qaSearchEntry from "./QAHandleSearchEntryReducer.js";
-import qa from "./QAReducer.js";
->>>>>>> 122b68b96a2848e074d04018c9f2ae1a0d647489
 import qaResultsArr from "./QAChangeResultsArrReducer.js";
 import qaIncrementer from "./QAIncrementerReducer.js";
 
 const rootReducer = combineReducers({
-<<<<<<< HEAD
     related,
     overviewProductInfo,
     overviewChangeStyles,
@@ -32,16 +21,6 @@ const rootReducer = combineReducers({
     reviews,
     qaResultsArr,
     qaIncrementer
-=======
-  related,
-  overviewProductInfo,
-  overviewChangeStyles,
-  overviewChangeSelectedStyles,
-  qaSearchEntry,
-  qa,
-  qaResultsArr,
-  qaIncrementer
->>>>>>> 122b68b96a2848e074d04018c9f2ae1a0d647489
 });
 
 export default rootReducer;
