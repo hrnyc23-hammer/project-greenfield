cd8c3dabd4b0a82475ceb1c1da49694d476eb7ac
Tom Cernera <tomcernera@gmail.com>
2019-07-18 15:07:34 -0400

    Basic material-ui grid

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid';
 
 const Overview = (props) => {
     return (
       <React.Fragment>
-        <div>Product Name : {props.info.name}</div>
-        <div>Category : {props.info.category}</div>
-        
-        {props.selectedStyle.sale_price > 0 ?
-        <div>Sale Price : {props.selectedStyle.sale_price}</div> : <div>Price : {props.selectedStyle.original_price}</div>}
-
+        <Grid container>
+        <Grid item xs={4}>
         <div>Selected: {props.selectedStyle.name}</div>
         <div><img src={props.selectedStyle.photos[0].thumbnail_url}/></div>
+        </Grid>
+        <Grid item xs={4}>
+        <div>Product Name : {props.info.name}</div>
+        <div>Category : {props.info.category}</div>
+
+        {props.selectedStyle.sale_price > 0 ?
+        <div>Sale Price : {props.selectedStyle.sale_price}</div> : <div>Price : {props.selectedStyle.original_price}</div>}
         <div>
           {props.styles.results.map(style => {
             return <div key={style.style_id} 
@@ -19,6 +23,8 @@ const Overview = (props) => {
                   </div>
           })}
         </div>
+        </Grid>
+        </Grid>
       </React.Fragment>
     )
 }
