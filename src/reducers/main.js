122b68b96a2848e074d04018c9f2ae1a0d647489
Kevin Park <kevypark@Kevins-MacBook-Pro.local>
2019-07-18 11:36:36 -0400

    got more answered questions button properly rendering out the qa.results array using slice

--- src/reducers/main.js
+++ src/reducers/main.js
@@ -1,18 +1,22 @@
-import { combineReducers } from 'redux';
-import related from './related.js';
-import overviewProductInfo from './overviewProductInfo.js';
-import overviewChangeStyles from './overviewChangeStyles';
-import overviewChangeSelectedStyles from './overviewChangeSelectedStyle.js';
-import qaSearchEntry from './QAHandleSearchEntryReducer.js';
-import qa from './QAReducer.js';
+import { combineReducers } from "redux";
+import related from "./related.js";
+import overviewProductInfo from "./overviewProductInfo.js";
+import overviewChangeStyles from "./overviewChangeStyles";
+import overviewChangeSelectedStyles from "./overviewChangeSelectedStyle.js";
+import qaSearchEntry from "./QAHandleSearchEntryReducer.js";
+import qa from "./QAReducer.js";
+import qaResultsArr from "./QAChangeResultsArrReducer.js";
+import qaIncrementer from "./QAIncrementerReducer.js";
 
 const rootReducer = combineReducers({
   related,
   overviewProductInfo,
   overviewChangeStyles,
-  overviewChangeSelectedStyles,  
+  overviewChangeSelectedStyles,
   qaSearchEntry,
-  qa
+  qa,
+  qaResultsArr,
+  qaIncrementer
 });
 
 export default rootReducer;
