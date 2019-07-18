b5f4cfb5aa4fd1be9a87d0a7ed58d9655e8c0df4
Kevin Park <kevypark@Kevins-MacBook-Pro.local>
2019-07-18 10:21:52 -0400

    merge conflict handled

--- src/reducers/main.js
+++ src/reducers/main.js
@@ -3,12 +3,16 @@ import related from './related.js';
 import overviewProductInfo from './overviewProductInfo.js';
 import overviewChangeStyles from './overviewChangeStyles';
 import overviewChangeSelectedStyles from './overviewChangeSelectedStyle.js';
+import qaSearchEntry from './QAHandleSearchEntryReducer.js';
+import qa from './QAReducer.js';
 
 const rootReducer = combineReducers({
   related,
   overviewProductInfo,
   overviewChangeStyles,
-  overviewChangeSelectedStyles
+  overviewChangeSelectedStyles,  
+  qaSearchEntry,
+  qa
 });
 
 export default rootReducer;
