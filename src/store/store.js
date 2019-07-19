62a3c17d3808133f829953d6d3d4eac4b2976800
lancerutkin <lance.rutkin@gmail.com>
2019-07-19 12:24:35 -0400

    Render basic modal functionality

--- src/store/store.js
+++ src/store/store.js
@@ -5,14 +5,6 @@ import sample from "../data/sampleItemData.js";
 
 var store = createStore(
   rootReducer,
-  {
-    info: sample.info,
-    styles: sample.styles,
-    related: sample.related,
-    qa: sample.qa,
-    reviews: sample.reviews,
-    meta: sample.meta
-  },
   applyMiddleware(thunkMiddleWare)
 );
 
