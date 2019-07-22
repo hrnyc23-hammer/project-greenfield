28e275d736b38c336459bcad8aa9173b0b9be5e9
Kevin Park <kevypark@Kevins-MacBook-Pro.local>
2019-07-22 18:15:49 -0400

    code cleanup

--- src/reducers/QAAddAQuestionReducer.js
+++ src/reducers/QAAddAQuestionReducer.js
@@ -1,9 +1,8 @@
 import Redux from "redux";
 
 var QAAddAQuestionReducer = (state = false, action) => {
-  // let currentState = state;
   switch (action.type) {
-    case  "QA_ADD_QUESTION_CLICKED":
+    case "QA_ADD_QUESTION_CLICKED":
       return action.clicked;
     default:
       return state;
