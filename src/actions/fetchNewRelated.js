148b59a119a844787fd4c03ab00182d03f592e8a
lancerutkin <lance.rutkin@gmail.com>
2019-07-21 23:09:17 -0400

    Fix bug importing wrong funciton to fetch related

--- src/actions/fetchNewRelated.js
+++ src/actions/fetchNewRelated.js
@@ -1,11 +1,11 @@
 import Axios from 'axios';
 import changeRelated from './changeRelated.js';
-import { getInfo, getStyles, getMeta } from '../infoFetchers';
+import { getProductInfo, getStyles, getMeta } from '../infoFetchers';
 
 const fetchNewRelated = (relatedIds) => {
   return (dispatch) => {
     Promise.all(relatedIds.map((id) => {
-      return Axios.all([getInfo(id), getStyles(id), getMeta(id)])
+      return Axios.all([getProductInfo(id), getStyles(id), getMeta(id)])
       .then(Axios.spread((infoResponse, stylesResponse, metaResponse) => {
         return {
           info: infoResponse.data,
