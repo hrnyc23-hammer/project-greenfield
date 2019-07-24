7f903dd4213f4ac1c3466eaa5bc6503db3ec0f91
lancerutkin <lance.rutkin@gmail.com>
2019-07-24 15:46:24 -0400

    Implement outfit scrolling

--- src/components/Outfits.jsx
+++ src/components/Outfits.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import OutfitItem from './OutfitItem';
@@ -11,12 +11,19 @@ const useStyles = makeStyles(theme => ({
   },
   add: {
     cursor: "pointer"
+  },
+  invisible: {
+    visibility: "hidden"
+  },
+  visible: {
+    visibility: "visible"
   }
 }));
 
 const Outfits = (props) => {
   const classes = useStyles();
   let outfits = {};
+
 
   useEffect(() => {
     outfits = JSON.parse(localStorage.getItem("greenfieldOutfits"))
@@ -28,7 +35,15 @@ const Outfits = (props) => {
     }
   });
 
-  
+  const [lowerLimit, setLowerLimit] = useState(0);
+
+  const reduceLimit = () => {
+    setLowerLimit(lowerLimit - 1);
+  };
+
+  const increaseLimit = () => {
+    setLowerLimit(lowerLimit + 1);
+  };
 
   const addOutfit = () => {
     if (outfits[props.info.id] === undefined) {
@@ -61,14 +76,32 @@ const Outfits = (props) => {
             <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
           </SvgIcon>
         </Grid>
+        <Grid item className={lowerLimit !== 0 ? classes.visible : classes.invisible}>
+          <SvgIcon color="primary" onClick={() => {
+            reduceLimit();
+            clickTracker("outfits", "compare");
+          }} className={classes.add}>
+            <path d="M11.67 3.87L9.9 2.1 0 12l9.9 9.9 1.77-1.77L3.54 12z"/>
+          </SvgIcon>
+        </Grid>
         {Object.keys(props.outfits).map((ele, idx) => {
-          return (
-            <Grid item key={idx}>
-              <OutfitItem item={props.outfits[ele]} removeFromOutfits={props.removeFromOutfits}/>
-            </Grid>
-          )
-        })
-      }  
+          if (idx >= lowerLimit && idx < lowerLimit + 4) {
+            return (
+              <Grid item key={idx}>
+                <OutfitItem item={props.outfits[ele]} removeFromOutfits={props.removeFromOutfits}/>
+              </Grid>
+              )
+            }
+          })
+        }
+        <Grid item className={lowerLimit + 4 < Object.keys(props.outfits).length ? classes.visible : classes.invisible}>
+          <SvgIcon color="primary" onClick={() => {
+            increaseLimit();
+            clickTracker("outfit", "compare");
+          }} className={classes.add}>
+            <path d="M5.88 4.12L13.76 12l-7.88 7.88L8 22l10-10L8 2z"/>
+          </SvgIcon>
+        </Grid>
       </Grid>
     </React.Fragment>
   )
