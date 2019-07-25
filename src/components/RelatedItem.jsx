e092583cc165aa9bfbd0f29adf18eb6f4849a4ab
lancerutkin <lance.rutkin@gmail.com>
2019-07-25 19:17:16 -0400

    Implement minor styling changes

--- src/components/RelatedItem.jsx
+++ src/components/RelatedItem.jsx
@@ -3,12 +3,10 @@ import ReviewsStars from './ReviewsStars';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import ComparisonModal from './ComparisonModal';
-import Button from '@material-ui/core/Button';
 import { clickTracker } from '../infoFetchers';
 import { SvgIcon } from '@material-ui/core';
 
@@ -78,7 +76,7 @@ const RelatedItem = props => {
             toggleOpen();
             clickTracker("compare-products", "compare");
           }}>
-            <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/>
+            <path d="M22 9.24l-7.19-.62L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27 18.18 21l-1.63-7.03L22 9.24zM12 15.4l-3.76 2.27 1-4.28-3.32-2.88 4.38-.38L12 6.1l1.71 4.04 4.38.38-3.32 2.88 1 4.28L12 15.4z"/>
           </SvgIcon>
           <ComparisonModal open={open} 
             handleClose={handleClose} 
